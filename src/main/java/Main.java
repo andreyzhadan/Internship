0ca330463cd7ea39d84aca5265182a02f202b980
andreyzhadan <azhadan@griddynamics.com>
2013-07-11 11:36:26 +0300

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,19 @@
+/**
+ * Created by azhadan on 7/10/13.
+ */
+public class Main {
+    public static void main(String[] args) {
+        Integer ref0 = new Integer(2);
+        Integer ref1 = new Integer(2);
+        Integer ref2 = ref1;
+        Number ref3 = ref2;
+        System.out.println((ref0 == ref1) + " / " + (ref1 == ref2) + " / " + (ref3 == ref1));
+
+
+        System.out.println(Integer.decode("50"));
+        int[] array = {10, 30, 20};
+        PrimitiveArray arr = new PrimitiveArray(array);
+        System.out.println(arr);
+
+    }
+}
---
+++ src/test/java/InternTest.java
@@ -0,0 +1,12 @@
+import org.junit.Test;
+
+/**
+ * Created by azhadan on 7/10/13.
+ */
+public class InternTest {
+
+    @Test
+    public void testEmpty() {
+        System.out.println("@Test - testEmptyCollection");
+    }
+}
