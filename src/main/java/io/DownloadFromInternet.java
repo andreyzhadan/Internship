c1f6fb5e7a10a7810c36ee908fcf735c27f845c7
andreyzhadan <azhadan@griddynamics.com>
2013-07-12 10:39:42 +0300

    Crawler finished

--- src/main/java/io/DownloadFromInternet.java
+++ src/main/java/io/DownloadFromInternet.java
@@ -5,30 +5,32 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.URL;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.HashSet;
+import java.util.Set;
 import java.util.Stack;
 
 /**
  * Created by azhadan on 7/11/13.
  */
 public class DownloadFromInternet {
-    private static final String domain = "http://skipy";
-    private static List<String> allLinks = new ArrayList<String>();
+    private static final String DOMAIN = "http://www.dataved.ru/2010/05";
+    private static Set<String> allLinks = new HashSet<String>();
     private static Stack<String> links = new Stack<String>();
 
     public static void main(String[] args) throws IOException {
-        String baseUrl = "http://skipy.ru/";
+        String baseUrl = "http://www.dataved.ru/2010/05/java.html";
         links.add(baseUrl);
         allLinks.add(baseUrl);
-        //while (!links.isEmpty())
-        crawl(links.remove(0));
+        while (!links.isEmpty())
+            crawl(links.pop());
     }
 
     public static void crawl(String urlString) throws IOException {
         URL url = new URL(urlString);
         InputStream in = url.openStream();
-        BufferedOutputStream bos = new BufferedOutputStream(new FileOutputStream("output/" + urlString.replaceAll("/", "")));
+        BufferedOutputStream bos = new BufferedOutputStream(new FileOutputStream("output/" +
+                urlString.substring(0, Math.min(urlString.length(), 255)).replaceAll("/", "").replaceAll("\\\\", "").replaceAll("\\?", "")));
+
         StringBuilder builder = new StringBuilder();
         int b;
         while ((b = in.read()) != -1) {
@@ -48,14 +50,15 @@ public class DownloadFromInternet {
             if (index2 != -1) {
                 String link = builder.substring(index, index2);
                 System.out.print(link);
-                if (link.contains(domain) && !allLinks.contains(link)) {
+                if (link.contains(DOMAIN) && !allLinks.contains(link)) {
                     links.push(link);
                     allLinks.add(link);
-                    System.out.print(" this link is in the same domain");
+                    System.out.print(" this link is in the same DOMAIN");
                 }
                 System.out.println("\n");
                 index = index2;
-            }
+            } else
+                break;
         }
     }
 }
